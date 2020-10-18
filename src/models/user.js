4f87f015f7ff678e909b638d878e869813d249e4
Marta <martagorpinchuk@gmail.com>
2020-10-18 14:29:43 +0300

    add fields to ProductSchema

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    prise: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        value: '',
+        required: true
+    },
+    provider: {
+        type: String,
+        required: false
+    },
+    expirationDate: {
+        type: Number,
+        required: true
+    },
+    dimension: {
+        type: String,
+        value: ''
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
 
 })
 
