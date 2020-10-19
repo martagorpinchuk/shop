2c3ef4dfa093a1ef26ad9e6cccf5cf3347eadd33
Marta <martagorpinchuk@gmail.com>
2020-10-19 14:49:02 +0300

    add function createProduct

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,29 @@
-const User = require('./../models/user');
-const _ = require('lodash');
-const { model } = require('../models/user');
+const Product = require('./../models/user');
+//const _ = require('lodash');
 
 module.exports = {
-    
+    createProduct
 } 
+
+async function createProduct(req, res, next) {
+    const fields = [
+        'name',
+        'prise',
+        'category',
+        'provider',
+        'expirationDate',
+        'dimension',
+        'quantity'
+    ];
+    const body = req.body;
+    const newProduct = _.pick(body, fields);
+    try {
+        const product = new Product(newProduct);
+        await product.save();
+        return res.status(200).json();
+    } 
+    catch(err) {
+        console.log(err);
+        next(err);
+    }
+}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 const userController = require('../controllers/user');
 
+router.post('/', userController.createProduct); 
+
 module.exports = router;
